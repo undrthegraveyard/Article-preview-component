183f7abe4d8029f090e1ac4382341bea7a1546de
Shivam <shivamagarwalmarch@gmail.com>
2024-07-09 15:44:53 +1000

    update

--- script.js
+++ script.js
@@ -2,27 +2,31 @@ function toggleActive() {
   const shareLinks = document.querySelector('.share-links');
   const profile = document.querySelector('.profile');
   const textDown = document.querySelector('.text-down');
-  
+
+  shareLinks.classList.toggle('active');
+
   if (window.innerWidth >= 700) {
-    shareLinks.classList.toggle('active');
     if (shareLinks.classList.contains('active')) {
       shareLinks.style.display = 'flex';
-     
+      shareLinks.setAttribute('tabindex', '-1');
+      shareLinks.focus();
     } else {
       shareLinks.style.display = 'none';
-      
+      shareLinks.removeAttribute('tabindex');
     }
   } else {
     profile.classList.toggle('hidden');
-    shareLinks.classList.toggle('active');
     if (shareLinks.classList.contains('active')) {
       shareLinks.style.display = 'flex';
       profile.style.display = 'none';
       textDown.style.background = "#48556a";
+      shareLinks.setAttribute('tabindex', '-1');
+      shareLinks.focus();
     } else {
       shareLinks.style.display = 'none';
       profile.style.display = 'flex';
-      textDown.style.background= '';
+      textDown.style.background = '';
+      shareLinks.removeAttribute('tabindex');
     }
   }
 }
